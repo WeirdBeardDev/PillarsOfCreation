2f1fc99589b5ee51117a660d13c9cfdbae101196
Aj Comfort <weirdbearddev@gmail.com>
2024-07-05 16:17:01 -0400

    feat(Character): Change attributes to zero for the default

--- src/Pillars/ClassLib/Player/Character.cs
+++ src/Pillars/ClassLib/Player/Character.cs
@@ -7,11 +7,11 @@ public class Character
 {
     public string Name { get; set; } = Data.NewCharacterName;
     public string ImageFileName { get; set; } = Data.DefaultImageFileName;
-    public AttributeScore Strength { get; set; } = new(10);
-    public AttributeScore Dexterity { get; set; } = new(10);
-    public AttributeScore Constitution { get; set; } = new(10);
-    public AttributeScore Intelligence { get; set; } = new(10);
-    public AttributeScore Wisdom { get; set; } = new(10);
-    public AttributeScore Personality { get; set; } = new(10);
+    public AttributeScore Strength { get; set; } = new(0);
+    public AttributeScore Dexterity { get; set; } = new(0);
+    public AttributeScore Constitution { get; set; } = new(0);
+    public AttributeScore Intelligence { get; set; } = new(0);
+    public AttributeScore Wisdom { get; set; } = new(0);
+    public AttributeScore Personality { get; set; } = new(0);
     public DateTime? LastPlayed { get; set; } = null;
 }
